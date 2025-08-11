d50259a9e39dab3bc70e262eeceaaf2666555e5d
waleedJadoon_personalAccount <waleedjadoon840@gmail.com>
2025-08-12 01:13:02 +0500

    Initial commit with .gitignore

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
